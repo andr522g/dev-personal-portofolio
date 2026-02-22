ff72dce2475fe53a0d1e9343c05b3d63394aaecf
André <pelle131201@gmail.com>
2026-02-22 22:03:15 +0100

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Testimonials } from "@/sections/Testimonials";
+import { Contact } from "@/sections/Contact";
+import { Navbar } from "@/components/Navbar";
+
 function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
-      <main></main>
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main>
     </div>
   )
 }
