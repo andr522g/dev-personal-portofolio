ae0b7d3452007118bce14af4d42acd6f4da8cb04
André <pelle131201@gmail.com>
2026-02-23 23:43:57 +0100

    added project array

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,38 @@
+export const projects = [
+  {
+    title: "Lake Stratification Submarine",
+    description:
+      "An automated submarine system that measures oxygen levels, temperature, and depth in lakes, transmitting data wirelessly to a server for real-time visualization through a web client.",
+    image: "/Submarine.png",
+    link: "https://github.com/andr522g/BaseStation.Server.git",
+    github: "https://github.com/andr522g/BaseStation.Server.git"
+  },
+  {
+    title: "Climate Regulation System",
+    description:
+      "A smart climate control system that monitors and regulates temperature and humidity using sensors, microcontroller logic, and an intuitive graphical user interface.",
+    image: "/KRS.png",
+    link: "#",
+    github: "#"  
+  },
+  {
+    title: "Self Driving Mini Car",
+    description:
+      "An embedded-system-based electric vehicle capable of autonomously navigating a track using sensor detection, motor control, lighting systems, and integrated audio feedback.",
+    image: "/MiniCar.png",
+    link: "#",
+    github: "#"
+  },
+  {
+    title: "Finance Tracker Application",
+    description:
+      "A full-stack web application for managing personal finances, featuring structured backend APIs, database integration, and interactive data visualization.",
+    image: "/FinanceTracker.png",
+    link: "https://github.com/a-chiha/FinanceTrackerBackend.git",
+    github: "https://github.com/a-chiha/FinanceTrackerBackend.git"
+  },
+];
+
 export const Projects = () => {
-    return <section></section>
-}
+  return <section></section>;
+};
