71e0eea102876f3579fe6b386b667633657f0c63
André <pelle131201@gmail.com>
2026-02-24 04:41:12 +0100

    fixing shit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Hero } from "@/sections/Hero";
 import { About } from "@/sections/About";
 import { Projects } from "@/sections/Projects";
 import { Experience } from "@/sections/Experience";
-import { Testimonials } from "@/sections/Testimonials";
 import { Contact } from "@/sections/Contact";
 import { Navbar } from "@/layout/Navbar";
 
